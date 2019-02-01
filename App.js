d608a13c5e6cfbac05a0ef5404b85e0f5688890b
LanceM26 <lancemagnaye@gmail.com>
2019-02-01 08:29:09 -0800

    New Repo from LanceMagnaye26/EmployeePortal

--- App.js
+++ App.js
@@ -7,8 +7,10 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Platform } from 'react-native';
+
+import { Input, Button, Text } from 'react-native-elements';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,12 +21,82 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accountNumber: "",
+      password: "",
+      buttonStatus: false,
+      signInDisabled: false,
+      inputFields: true,
+      passwordRequired: null,
+      accountNumberRequired: null,
+    }
+  }
+
+  signIn = () => {
+
+    let accountNumber = this.state.accountNumber;
+    let password = this.state.password
+
+    if (accountNumber.trim() === "") {
+      this.setState({ accountNumberRequired: "required" });
+      return
+    } else {
+      this.setState({ accountNumberRequired: null });
+    }
+    if (password.trim() === "") {
+      this.setState({ passwordRequired: "required" });
+      return
+    } else {
+      this.setState({ passwordRequired: null });
+    }
+    this.setState({
+      buttonStatus: true,
+      signInDisabled: true,
+      inputFields: false,
+    });
+    //verify login with database
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.textContainer} h3>Unplug and Thrive</Text>
+
+
+        <View style={styles.form}>
+          <Input
+            placeholder='Employee Number'
+            shake={true}
+            editable={this.state.inputFields}
+            inputStyle={{ color: "rgba(253,255,252,1)" }}
+            errorStyle={{ color: 'red' }}
+            onChangeText={(text) => this.setState({ accountNumber: text })}
+            errorMessage={this.state.accountNumberRequired}
+          />
+
+          <Input
+            placeholder='Password'
+            shake={true}
+            secureTextEntry={true}
+            editable={this.state.inputFields}
+            inputStyle={{ color: "rgba(253,255,252,1)" }}
+            errorStyle={{ color: 'red' }}
+            onChangeText={(text) => this.setState({ password: text })}
+            errorMessage={this.state.passwordRequired}
+          />
+          <Button
+            title="Sign In"
+            disabled={this.state.signInDisabled}
+            disabledStyle={{ backgroundColor: '#0b3954' }}
+            buttonStyle={{ backgroundColor: '#0b3954' }}
+            containerStyle={styles.button}
+            loading={this.state.buttonStatus}
+            titleStyle={{ color: "rgba(253,255,252,1)" }}
+            onPress={() => { this.signIn() }}
+          />
+        </View>
       </View>
     );
   }
@@ -35,16 +107,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#0b3954',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  form: {
+    backgroundColor: "rgba(253,255,252, 0.5)",
+    width: "80%",
+    padding: "5%",
+    borderRadius: 20,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  button: {
+    marginTop: 10,
   },
+
+  textContainer: {
+    marginHorizontal: '5%',
+    color: "rgba(253,255,252,1)",
+    marginBottom: "10%",
+    fontWeight: 'bold',
+  },
+
 });
