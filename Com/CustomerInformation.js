bb839bb51ced312f243fcd88bba6b85f89aa33bf
japj91 <japjohal91@gmail.com>
2019-02-13 15:09:32 -0800

    touch id working

--- Com/CustomerInformation.js
+++ Com/CustomerInformation.js
@@ -6,9 +6,20 @@ import {  StyleSheet,  View, FlatList, SafeAreaView, TouchableWithoutFeedback } 
 import { Text, Button, Image, ListItem} from 'react-native-elements';
 
 export default class CustomerInformation extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            data:[{name:"jason",key:"1"},{name:"jap",key:"2"},{name:"jason",key:"3"},{name:"jap",key:"4"},{name:"iii",key:"5"},{name:"bb",key:"6"},{name:"qqq",key:"7"},{name:"ddd",key:"8"},{name:"ggg",key:"9"},{name:"lll",key:"10"},{name:"opopo",key:"11"},{name:"adsf",key:"12"}],
+            pic: "../avatars/ava.png"
+        }
+        this.ClickMe = this.ClickMe.bind(this);
+    }
+
     static navigationOptions = {
         title: 'Customer Info',
     }
+
+
     render(){
         const { navigation } = this.props;
         const name = navigation.getParam("name","Jap")
@@ -17,6 +28,9 @@ export default class CustomerInformation extends Component {
             <View>
                 <Text>Hello {JSON.stringify(name)}</Text>
                 <Text>Hello {JSON.stringify(id)}</Text>
+                <Button
+                title="Clickme"
+                onPress={this.ClickMe}/>
             </View>
         )
     }
